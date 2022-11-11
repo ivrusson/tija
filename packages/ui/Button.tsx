ab2acaaaa4d30dd22fe60ab99b0d1ae3095891af
ivrusson <ivrusson@gmail.com>
2022-11-11 22:23:15 +0100

    Initial commit

---
+++ packages/ui/Button.tsx
@@ -0,0 +1,10 @@
+export const Button = () => {
+  return (
+    <button
+      type="button"
+      className="rounded-8xl h-16 w-16 bg-blue-900 hover:bg-blue-700"
+    >
+      Boop
+    </button>
+  )
+}
